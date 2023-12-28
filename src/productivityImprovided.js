09909101f30129ad1b2b5db321e56e96e249aaf4
Victor Adeshile <davik4life@gmail.com>
2023-12-28 23:48:03 +0100

    Added Productivity Note

---
+++ src/productivityImprovided.js
@@ -0,0 +1,20 @@
+// div > (header > p > ol > li * 4) + footer > p >
+// The above code makes it possible to write codes faster and I think I love the trick. Really awesome.
+// Same code is what expands to for the structure below. Improves productivity.
+
+// Making it a tradition when writing any JSX or HTML structure within my REACT code or any other code.
+<div>
+	<header>
+		<p>
+			<ol>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+			</ol>
+		</p>
+	</header>
+	<footer>
+		<p></p>
+	</footer>
+</div>;

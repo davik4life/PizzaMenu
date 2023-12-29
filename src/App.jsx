9c842434e8be1d85bddb4f5d230fc976f4050337
Victor Adeshile <davik4life@gmail.com>
2023-12-29 02:07:09 +0100

    Added Browser Alerts function

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 export default function App() {
 	return (
 		<>
-			<h1> Welcome To The Pizza Shop</h1>
+			{/* <h1> Welcome To The Pizza Shop</h1> */}
 			<Header />
 			<Menu />
 			<Footer />
@@ -15,7 +15,7 @@ export default function App() {
 }
 
 function Header() {
-	return <h1>Fast React Pizza Company</h1>;
+	return <h1>Fast React Pizza Co.</h1>;
 }
 
 function Menu() {
@@ -31,7 +31,13 @@ function Menu() {
 }
 
 function Footer() {
-	return <h1>{new Date().toLocaleTimeString()} We're currently Open!!!</h1>;
+	const hour = new Date().getHours();
+	const openHour = 12;
+	const closedHour = 22;
+	return hour >= openHour && hour <= closedHour
+		? alert("We're currently Open!!!")
+		: alert("We're currently Closed!!!");
+	// return <h1>{new Date().toLocaleTimeString()} We're currently Open!!!</h1>;
 }
 function Pizza() {
 	return (

e17317d2ab5a1872b00249d65e15177cb0b3b47e
Victor Adeshile <davik4life@gmail.com>
2023-12-30 13:13:52 +0100

    Improved on the dev

--- src/App.jsx
+++ src/App.jsx
@@ -5,28 +5,53 @@ import "./index.css";
 
 export default function App() {
 	return (
-		<>
+		<div className="container">
 			{/* <h1> Welcome To The Pizza Shop</h1> */}
 			<Header />
 			<Menu />
 			<Footer />
-		</>
+		</div>
 	);
 }
 
 function Header() {
-	return <h1>Fast React Pizza Co.</h1>;
+	return (
+		<header className="header">
+			<h1>Fast React Pizza Co.</h1>;
+		</header>
+	);
 }
 
 function Menu() {
 	return (
-		<>
+		<main className="menu">
 			<h2>Our Menu</h2>
-			<Pizza />
-			<Pizza />
-			<Pizza />
-			<Pizza />
-		</>
+			<Pizza
+				name="Pizza Funghi"
+				photoName="/pizzas/funghi.jpg"
+				ingredients="Butter, Pure Flour, Salt, Sugar, Salty Cream"
+				price={10}
+			/>
+			<Pizza
+				name="Focaccia"
+				photoName="/pizzas/focaccia.jpg"
+				ingredients="Cheeze, Pure Flour, Salt, Sugar, Cream"
+				price={20}
+			/>
+		</main>
+	);
+}
+
+function Pizza(props) {
+	return (
+		<div className="pizza">
+			<img src={props.photoName} alt={props.name} />
+			<div>
+				<h3>{props.name}</h3>
+				<p>{props.ingredients}</p>
+				<span>{props.price}</span>
+			</div>
+		</div>
 	);
 }
 
@@ -34,17 +59,9 @@ function Footer() {
 	const hour = new Date().getHours();
 	const openHour = 12;
 	const closedHour = 22;
-	return hour >= openHour && hour <= closedHour
-		? alert("We're currently Open!!!")
-		: alert("We're currently Closed!!!");
-	// return <h1>{new Date().toLocaleTimeString()} We're currently Open!!!</h1>;
-}
-function Pizza() {
-	return (
-		<>
-			<img src="/pizzas/funghi.jpg" alt="Nice Pizza" />
-			<h2>Delicious Pizza available today! </h2>
-			<p>A taste would leave you coming back for more!!!</p>
-		</>
-	);
+	const openOrClosed =
+		hour >= openHour && hour <= closedHour
+			? "We're currently Open!!!"
+			: "We're currently Closed!!!";
+	return <h1 className="footer">{openOrClosed}</h1>;
 }
